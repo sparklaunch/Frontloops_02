5628adb2edd5adcfac6cdbbf2c7a5982f967437a
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-02-28 14:29:00 +0900

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+import "./SimpleWheelsTitle.dart";
+
+Color backgroundColor = Color.fromRGBO(22, 149, 122, 1);
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: backgroundColor,
         appBar: AppBar(
           title: Text("Frontloops 02"),
         ),
@@ -16,6 +21,31 @@ void main() {
 class Frontloops extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: SimpleWheelsTitle(),
+          width: 350.0,
+          margin: EdgeInsets.only(
+            top: 50.0,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              10.0,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black38,
+                offset: Offset(5.0, 5.0),
+                spreadRadius: 8.0,
+                blurRadius: 8.0,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
